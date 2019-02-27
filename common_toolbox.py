e1e539badd5ec411f3b732b91d340807eb3afbc0
Jakob Ejler Sørensen <lillebror.jakobes@gmail.com>
2019-02-27 17:19:54 +0100

    Optionalize axis argument in flip_x and add smooth function

--- common_toolbox.py
+++ common_toolbox.py
@@ -6,8 +6,10 @@ import matplotlib.pyplot as plt
 
 colors = ['k', 'r', 'g', 'b', 'm', 'y', 'c']*5
 
-def flip_x(ax=plt.gca()):
+def flip_x(ax=None):
     """Flip the axis of `Axes` object ax"""
+    if ax is None:
+        ax = plt.gca()
     x1, x2 = ax.get_xlim()
     ax.set_xlim([x2, x1])
 
@@ -23,3 +25,15 @@ def copy_range(x, lim1, lim2):
     x_copy = x[np.where(x >= lim1)[0]]
     x_copy = x_copy[np.where(x_copy <= lim2)[0]]
     return x_copy
+
+def smooth(data, width=1):
+    """Average `data` with `width` neighbors"""
+    smoothed_data = np.zeros(len(data))
+    smoothed_data[width:-width] = data[2*width:]
+    for i in range(2*width):
+        smoothed_data[width:-width] += data[i:-2*width+i]
+        if i < width:
+            smoothed_data[i] = sum(data[0:i+width+1])/len(data[0:i+width+1])
+            smoothed_data[-1-i] = sum(data[-1-i-width:])/len(data[-1-i-width:])
+    smoothed_data[width:-1-width] = smoothed_data[width:-1-width]/(2*width+1)
+    return smoothed_data
