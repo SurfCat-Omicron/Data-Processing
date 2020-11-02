9c640005e7a599e0abb6fdabe09a97b012be61c7
Julius Needham <73484189+JuliusNeedham@users.noreply.github.com>
2020-11-02 10:35:02 +0100

    Can now plot any ISS/XPS spectrum
    Fixed an error where only first scan could be plotted.

--- shortplotbrowse.py
+++ shortplotbrowse.py
@@ -1,118 +1,202 @@
-##Up to date plotting directly from vamas files.
+##A toolbox for plotting from VAMAS files. Works with ISS and XPS data.
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 from tkinter import *
 from tkinter import filedialog
 
-root = Tk()
-cdir = os.getcwd()
-files = filedialog.askopenfilenames(initialdir = cdir, title = 'Select files to plot', filetypes = [('Vamas files', '*.vms')])
-filesread = root.tk.splitlist(files)
-filename= []
-for i in filesread:
-    filename.append(i.split('/')[-1])
+#Determine whether data is XPS or ISS and passes the appropriate loaded module and an indicative marker. This is used in other functions throughout the code.
+def xps_or_iss(): 
+    if filename[0].split('_')[3].split('-')[0] == 'XPS':
+        import XPS as module
+        marker = 0 
+    elif filename[0].split('_')[3].split('-')[0] == 'ISS':
+        import ISS as module
+        marker = 1
+    return module, marker
     
-# Change working directory to directory first chosen file is in.
-os.chdir(filesread[0].replace(filename[0],'')) 
+#Load data with a file prompt, change the working directory to it, and return the filename
+def loaddata():
+    root = Tk()
+    cdir = os.getcwd()
+    files = filedialog.askopenfilenames(initialdir = cdir, title = 'Select files to plot', filetypes = [('Vamas files', '*.vms')])
+    filesread = root.tk.splitlist(files)
+    filename= []
+    root.destroy()
+    for i in filesread:
+        filename.append(i.split('/')[-1])
 
-## If more than one filename is written then this if-statement is used.##
+    # Change working directory to directory first chosen file is in.
+    os.chdir(filesread[0].replace(filename[0],'')) 
+    return filename
 
-#filename = []
-#for i in range(len(sys.argv)-1):
-#    filename.append(sys.argv[i+1])
+# Plotting function        
+def plottingtool():
+    module = xps_or_iss()[0]
+    marker = xps_or_iss()[1]
 
-if filename[0].split('_')[3].split('-')[0] == 'XPS':
-    import XPS as module
-elif filename[0].split('_')[3].split('-')[0] == 'ISS':
-    import ISS as module
+    # Creating empty arrays for appending information later.
+    sample = []
+    comment = []
+    region = []
+    date = []
+    current_regions= []
+    
+    # Loop that loads the data in from all the files. The module has already
+    # been set so if the files are not all XPS or all ISS, this will fail.
+    for index, k in enumerate(filename):
+        # Information on file k. Specifically the type of scan,
+        # regions, samples, comments and dates.
+        fileinfo = k.split('_')
+        type = fileinfo[3].split('-')[0]
+        region.append(fileinfo[3].split('-')[2])
+        current_region = int(region[index])-1
+        current_regions.append(current_region)
 
-    ## Creating empty arrays for appending information later.
-sample = []
-comment = []
-region = []
+        sample.append(fileinfo[1].split('-')[1])
+        comment.append(fileinfo[1].split('-')[0])
+        date.append(fileinfo[0].split('-')[0])    
+        
+        #Allow plotting of other than first scan - region not 1 (files with index 1)
+        region_not_1 = False
+        if not region[index] == '1': 
+            k = fileinfo[0] + '_' + fileinfo[1] + '_' + fileinfo[2] + '_' + type + '-' + '-' + '1' + '_' + fileinfo[4] + '_' + fileinfo[5]
+            region_not_1 = True
+            
+        # Load the data using either the XPS or ISS module for the
+        # specific file k.
+        exp = module.Experiment(k)
+        
+        # Create x,y arrays and append the data into them. Each scan enters as
+        # an array that is appended. X-axis depends on the type identified
+        # above.
+        if index == 0:
+            x, y = [], []
+        if marker == 0: #xps
+            x.append(exp.BE[current_region][::-1])
+            y.append(exp.cps[current_region][::-1])
+        elif marker == 1: #iss
+            x.append(exp.energy[current_region])
+            y.append(exp.cps[current_region])  
+            
+    # Create figure and assign y-axis label.
+    fig = plt.figure(1)
+    plt.ylabel('Counts')
 
-    ##Loop that loads the data in from all the files. The module has already
-    ## been set so if the files are not all XPS or all ISS, this will fail.
-    ##
+    # Assign the appropriate label depending on data type and scale to the x-axis.
+    if marker == 0: #xps
+        plt.xlabel('Binding Energy [eV]')
+        plt.xlim(x[0][-1],x[0][0])
+        plt.xticks(np.arange(round(x[0][0]/100)*100,x[0][-1], step = 100))
+    elif marker== 1: #iss
+        plt.xlabel('Kinetic Energy [eV]')
+        plt.xticks(np.arange(round(x[0][0]/100)*100,x[0][-1], step = 100))
 
-for index, k in enumerate(filename):
-
-        ## Finds information on file k. Specifically the type of scan,
-        ## regions, samples, comments and dates.
-    fileinfo = k.split('_')
-    type = fileinfo[3].split('-')[0]
-    region.append(fileinfo[3].split('-')[2])
-    sample.append(fileinfo[1].split('-')[1])
-    comment.append(fileinfo[1].split('-')[0]+'-')
-    date = fileinfo[0].split('-')[0]
-
-        ##Load the data using either the XPS or ISS module for the
-        ## specific file k.
-    exp = module.Experiment(k)
-        ##Create x,y arrays and append the data into them. Each scan enters as
-        ## an array that is appended. X-axis depends on the type identified
-        ## above.
-    if index == 0:
-        x, y = [], []
-    if type.upper() == 'XPS':
-        x.append(exp.BE[0][::-1])
-        y.append(exp.cps[0][::-1])
-        #y=[i+index*1000 for i in y]
-
-    elif type.upper() == 'ISS':
-        x.append(exp.energy[0])
-        y.append(exp.cps[0])
-
-    #Create figure and assign y-axis label.
-fig = plt.figure(1)
-plt.ylabel('Counts')
-
-    ## Assign the appropriate label and scale to the x-axis.
-if type.upper() == 'XPS':
-    plt.xlabel('Binding Energy [eV]')
-        #v=[-5, 1000, 0, np.amax(y)+200]
-    plt.xlim(x[0][-1],x[0][0])
-    plt.xticks(np.arange(round(x[0][0]/100)*100,x[0][-1], step = 100))
-elif type.upper() == 'ISS':
-    plt.xlabel('Kinetic Energy [eV]')
-    #v=[x[0][0], x[0][-1], 0, np.amax(y)+100]
-    plt.xticks(np.arange(round(x[0][0]/100)*100,x[0][-1], step = 100))
-    #plt.axis(v)
-
-    ## The plotting of all the scans in the loaded files.
-for i in range(len(filename)):
-    if type.upper() == 'XPS':
-        yplot=[q+i*20000 for q in y[i]]
-    elif type.upper() == 'ISS':
-        yplot=[q+i*0 for q in y[i]]
-        ysave = np.array(y[i])
-        fname = 'plot'+str(i)+'.txt'
-        #np.savetxt(fname,ysave)
-    #np.savetxt('x.txt', x[i])
-    plt.plot(x[i],yplot)
+    # The plotting of all the scans in the loaded files.
+    for i in range(len(filename)):
+        if type.upper() == 'XPS':
+            yplot=[q+i*20000 for q in y[i]]
+        elif type.upper() == 'ISS':
+            yplot=[q+i*0 for q in y[i]]
+            ysave = np.array(y[i])
+            fname = 'plot'+str(i)+'.txt'
+        plt.plot(x[i],yplot)
 
 
 
-    #Fix comments and sample names for file and title.
-samples = ''
-samples2 = ''
-comments = ''
+    # Create arrays for plot naming.
+    # Comments are seperated by + and dates are seperated by + and are reformatted to yyyy-mm-dd format
+    # (easier to sort in file explorer than dd-mm-yyyy)
+    comments = ''
+    dates = ''
 
-for i in range(len(sample)):
-    if sample[i] not in samples:
-            ## for figure title.
-        samples = (samples + sample[i]+ ' ')
-        ## for saved file name.
-        samples2 = (samples2 + sample[i])
+    for i in range(len(comment)):
+        comments += comment[i] + '+'
+    comments = comments.rstrip('+')
+    for i in range(len(date)):
+            dates += date[i][:4] + '-' + date[i][4:6] + '-' + date[i][6:8] + '+'
+    dates = dates.strip('+')        
+      
+    # Create figure with legend and title, which changes depending on amount of data chosen.
+    if comment.count(comment[0]) == len(comment): #If all comments are same, set legend to sample and title to comment
+        plt.legend(sample)
+        plt.title(comment[0])
+    else:
+        plt.legend(comments.split('+'))
+        plt.title(sample)
+        
+    #Change title depending on single plot or multiplot. Mostly for use during saving.    
+    if len(comment)==1:
+        fig.canvas.set_window_title(dates[0] + '_' + comment[0] + '_' + sample[0] + '_' + type + '_' + region[0])
+    elif len(comment)>1:    
+        fig.canvas.set_window_title('Multiplot_' + dates + '_' + comments + '_' + type)
+    
+    #Show the plot
+    plt.show()
 
-for i in range(len(comment)):
-    comments += comment[i] + ' '
-    ## Create figure title and save figure to file.
-plt.title(samples+ 'Scans: '+comments)
-commentslist = comments.split('-')
-plt.legend(commentslist)
-plt.savefig(type + samples2 +'.png',dpi = 600)
+#Saving data as .txt (with Origin compatible header)
+def savingtool():
+    for i in range(len(filename)):
+        #Remove any subfolder designation from file name.
+        filename[i] = filename[i].lstrip('\b')
+        
+        #Check whether files are XPS or ISS, and loads appropriate module, sets marker and prints detected type.
+        module = xps_or_iss()[0]
+        marker = xps_or_iss()[1]
+        #print('Detected type: ' + filename.split('_')[3].split('-')[0])
+        
+        #Import VAMAS and save as .txt with header in same folder.
+        exp = module.Experiment(filename[i])
+        for number in exp.cps.keys():
+            #print('Writing region {}'.format(number))
+            new_filename = filename[i].split('--')[0] + '_Region_{}.txt'.format(number+1)
+            f = open(new_filename, 'w')
+            f.writelines(['Energy,Counts/s\neV,A.U.\n'])
+            if marker == 0: # xps
+                x = exp.BE[number]
+            elif marker == 1: # iss
+                x = exp.energy[number]
+            y = exp.cps[number]
+            for j in range(len(y)):
+                line = '{},{}\n'.format(x[j], y[j])
+                f.write(line)
+            print('File saved as: {}'.format(new_filename))
+            f.close()
 
-plt.show()
-#print(x)
+
+# ------------------------------------------------------------------------------------
+# Actual script begins here
+# ------------------------------------------------------------------------------------
+
+#When script first runs, data is loaded and plotted
+filename = loaddata()
+plottingtool()
+
+#Simple menu
+ans=True
+while ans:
+    print("""
+    -------------------------------
+    Welcome to the XPS/ISS toolbox!
+    -------------------------------
+    
+    Options:
+    -------------------------------
+    1. Load other data
+    2. Plot data
+    3. Save data as .txt
+    4. Quit
+    -------------------------------
+    """)
+    ans=input("Choose an option: ")
+    if ans=="1":
+      filename = loaddata() #overwrites current filename and changes working directory
+    elif ans=="2":
+      plottingtool() 
+    elif ans=="3":
+      savingtool()
+    elif ans=="4":
+      ans = None #Quits the script
+    else:
+       print("\n Not a valid choice, try again")
+
